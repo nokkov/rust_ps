3de506534654e28a42d9d27e764f16b6afe4480b
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2025-06-05 00:35:17 +0300

    add elapsed time

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::ffi::OsString;
-
+use chrono::{DateTime, Utc};
 use sysinfo::System;
 
 fn main() {
@@ -9,30 +9,34 @@ fn main() {
     // println!("=> system:");
     // println!("System hostname: {:?}", System::host_name().unwrap());
 
-    println!("[pid] [name] [cpu_usage] [read_bytes] [written_bytes] [start_time] [status] [cmd]");
+    println!("[pid] [name] [cpu_usage] [read_bytes] [written_bytes] [elapsed_time] [status] [cmd]");
 
     for (pid, process) in sys.processes() {
-        let p_name = process.name().to_str().unwrap(); // add checks
+        let p_name = process.name().to_str().unwrap();
         let p_cpu_usage = process.cpu_usage();
 
         let p_disk_usage = process.disk_usage();
         let p_read_bytes = p_disk_usage.total_read_bytes;
         let p_written_bytes = p_disk_usage.total_written_bytes;
 
-        let p_start_time = process.start_time(); // convert
+        let p_start_time_timestamp = process.start_time().to_string().parse::<i64>().unwrap();
+        let p_start_time_date = DateTime::from_timestamp(p_start_time_timestamp, 0).unwrap();
+        let current_date_time = Utc::now();
+        let p_elapsed_time = current_date_time - p_start_time_date;
+
         let p_status = process.status();
 
         let p_cmd_os_string= process.cmd().join(&OsString::from(" "));
         let p_cmd = p_cmd_os_string.to_str().expect("Error during converting osstr -> str");
 
         println!(
-            "{0} {1} {2} {3} {4} {5} {6} {7}", 
+            "{0} | {1} | {2} | {3} | {4} | {5} | {6} | {7}", 
             pid, 
             p_name, 
             p_cpu_usage, 
             p_read_bytes,
             p_written_bytes, 
-            p_start_time,
+            p_elapsed_time,
             p_status, 
             p_cmd
         )

ec423eb8e110714a940a572e7f99434baa9e57b2
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2025-06-08 14:47:01 +0300

    add system name

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,7 @@ fn main() {
     let mut sys = System::new();
     sys.refresh_all();
 
-    // println!("=> system:");
-    // println!("System hostname: {:?}", System::host_name().unwrap());
+    println!("System hostname: {:?}", System::host_name().unwrap());
 
     println!(
         "{0: <10} | {1: <10} | {2: <10} | {3: <10} | {4: <10} | {5: <10} | {6: <10} | {7: <10}",

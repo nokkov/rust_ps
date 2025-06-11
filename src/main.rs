13abab7a73febb813ee5a37dba281f75e560ade4
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2025-06-11 22:31:47 +0300

    add draw_ui and run functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::{ffi::{OsStr, OsString}, io::{self, Write}, thread, time::Duration};
+use std::{error::Error, ffi::{OsString}, io::{self, stdout, Write}, thread, time::Duration};
 use chrono::{DateTime, Utc};
 use sysinfo::{Process, ProcessStatus, System};
-use termion::{clear, cursor};
-
-//FIXME: &str or String?
+use termion::{clear, cursor, screen::IntoAlternateScreen};
 
 const REFRESH_DURATION: Duration = Duration::from_secs(2);
 const PROCESS_LIMIT: usize = 15;
@@ -56,59 +54,52 @@ fn gather_process_data(sys: &System) -> Vec<ProcessInfo> {
             .collect()
 }
 
-fn main() {
-    let mut sys = System::new();
-    let mut stdout = io::stdout().lock();
+fn draw_ui<W: Write>(screen: &mut W, processes: &[ProcessInfo]) -> io::Result<()> {
+    write!(screen, "{}{}", cursor::Goto(1, 1), clear::All)?;
 
-    //TODO: alternate screen
+    writeln!(screen, "System Hostname: {}\r", System::host_name().unwrap_or_else(|| "N/A".to_string()))?;
 
-    write!(stdout, "{}{}", cursor::Goto(1, 1), clear::All).unwrap();
-    write!(stdout, "System hostname: {:?}\n", System::host_name().unwrap_or_else(|| "N/A".to_string())).unwrap();
-    write!(stdout, "{0: <10} | {1: <10} | {2: <10} | {3: <10} | {4: <10} | {5: <10} | {6: <10} | {7: <10}",
-    "PID", "NAME", "CPU%", "READ(B)", "WRITTEN(B)", "ELAPSED(MIN)", "STATUS", "CMD").unwrap();
+    writeln!(screen, "{0: <width$} | {1: <width$} | {2: <width$} | {3: <width$} | {4: <width$} | {5: <width$} | {6: <width$} | {7: <width$}\r",
+        "PID", "NAME", "CPU %", "READ(B)", "WRITTEN(B)", "UPTIME(m)", "STATUS", "COMMAND", width = COL_WIDTH)?;
+    writeln!(screen, "{}\r", "─".repeat(COL_WIDTH * 8 + 7 * 3))?;
 
-    stdout.flush().unwrap();
-    
-    loop {
-
-        write!(stdout, "{}{}", cursor::Goto(1, 5), clear::AfterCursor).unwrap(); 
-        stdout.flush().unwrap();
-
-        sys.refresh_all();
-
-        for (pid, process) in sys.processes().iter().take(10) {
-            let p_name = process.name().to_str().unwrap();
-            let p_cpu_usage = process.cpu_usage();
-
-            let p_disk_usage = process.disk_usage();
-
-            let p_read_bytes = p_disk_usage.total_read_bytes;
-            let p_written_bytes = p_disk_usage.total_written_bytes;
-            
-            let p_start_time_timestamp = process.start_time().to_string().parse::<i64>().unwrap();
-            let p_start_time_date = DateTime::from_timestamp(p_start_time_timestamp, 0).unwrap();
-            let current_date_time = Utc::now();
-            let p_elapsed_time = (current_date_time - p_start_time_date).num_minutes();
-            
-
-            let p_status = process.status();
-
-            let p_cmd_os_string= process.cmd().join(&OsString::from(" "));
-            let p_cmd = p_cmd_os_string.to_str().expect("Error during converting osstr -> str");
-
-            println!(
-                "{0: <10} | {1: <10} | {2: <10} | {3: <10} | {4: <10} | {5: <10} | {6: <10} | {7: <10}", 
-                truncate_and_ellipsis(&pid.to_string(), 10), 
-                truncate_and_ellipsis(&p_name.to_string(), 10),
-                truncate_and_ellipsis(&p_cpu_usage.to_string(), 10), 
-                truncate_and_ellipsis(&p_read_bytes.to_string(), 10),
-                truncate_and_ellipsis(&p_written_bytes.to_string(), 10), 
-                truncate_and_ellipsis(&p_elapsed_time.to_string(), 10),
-                truncate_and_ellipsis(&p_status.to_string(), 10), 
-                truncate_and_ellipsis(&p_cmd.to_string(), 10)
-            )
-        }
-
-        thread::sleep(Duration::from_secs(5)); // TODO: custom refresh time
+    for p_info in processes {
+        
+        writeln!(
+            screen,
+            "{0: <width$} | {1: <width$} | {2: <width$} | {3: <width$} | {4: <width$} | {5: <width$} | {6: <width$} | {7: <width$}\r",
+            truncate_and_ellipsis(&p_info.pid.to_string(), COL_WIDTH),
+            truncate_and_ellipsis(&p_info.name.to_os_string().into_string().unwrap(), COL_WIDTH),
+            truncate_and_ellipsis(&p_info.cpu_usage.to_string(), COL_WIDTH),
+            truncate_and_ellipsis(&p_info.read_bytes.to_string(), COL_WIDTH),
+            truncate_and_ellipsis(&p_info.written_bytes.to_string(), COL_WIDTH),
+            truncate_and_ellipsis(&p_info.elapsed_time.to_string(), COL_WIDTH),
+            truncate_and_ellipsis(&p_info.status.to_string(), COL_WIDTH),
+            truncate_and_ellipsis(&p_info.cmd.to_os_string().into_string().unwrap(), COL_WIDTH * 2),
+            width = COL_WIDTH
+        )?;
     }
+
+    screen.flush()
+}
+
+fn run<W: Write>(screen: &mut W, sys: &mut System) -> Result<(), Box<dyn Error>> {
+    loop {
+        sys.refresh_all();
+        let processes = gather_process_data(sys);
+        draw_ui(screen, &processes)?;
+        thread::sleep(REFRESH_DURATION);
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut sys = System::new_all();
+    let mut screen = stdout().into_alternate_screen()?;
+    
+    if let Err(e) = run(&mut screen, &mut sys) {
+        eprintln!("Application error: {}", e);
+        return Err(e);
+    }
+    
+    Ok(())
 }
